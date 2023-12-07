0782449bbb2022008271bce81c23d91aca2d3933
Aleksandr White <aleksandr@asdfjackal.com>
2023-12-07 11:09:10 -0500

    Day 8 setup

---
+++ day-08/src/bin/part1.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let input = include_str!("../input1.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> String {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1() {
+        let input = include_str!("../test1.txt");
+        assert_eq!("answer", part1(input));
+    }
+}
